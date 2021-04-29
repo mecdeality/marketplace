ab3bbd2c0dd179e1210a6b21ccb95d20beffe5dd
mecdeality <utegen.2019@gmail.com>
2021-04-30 01:02:46 +0600

    added models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    picture_url = models.CharField(default='https://pixy.org/src/78/788566.jpg', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    description = models.CharField(max_length=200)
+    seller = models.CharField(max_length=100)
+    price = models.IntegerField()
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.game
