1d21d10480cd1dc3c91922b1f61a8ec25d5bd3c0
mecdeality <utegen.2019@gmail.com>
2021-04-30 13:32:19 +0600

    some updates

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name="product-main"),
-    path('<int:game_id>', views.game_items, name='game-items')
+    path('<int:game_id>', views.game_items, name='game-items'),
+    path('<int:game_id>/<str:seller_name>', views.sellers_item, name='sellers-item')
 ]
--- product/views.py
+++ product/views.py
@@ -4,17 +4,42 @@ from django.http import HttpResponse
 
 
 def main(request):
+    if request.method == 'POST':
+        # return HttpResponse('dadw')
+        search_string = request.POST.get('search', '')
+        games = Game.objects.all().filter(name__contains=search_string)
+    else:
+        games = Game.objects.all()
+
     context = {
         'title': 'Main Page',
-        'games': Game.objects.all()
+        'search_text': 'Search by games',
+        'games': games
     }
     return render(request, 'product/main.html', context)
 
 
 def game_items(request, game_id):
     game = Game.objects.filter(id=game_id).first()
+    if request.method == 'POST':
+        # return HttpResponse('dadw')
+        search_string = request.POST.get('search', '')
+        items = Item.objects.all().filter(game=game_id, description__contains=search_string)
+    else:
+        items = Item.objects.all().filter(game=game_id)
     context = {
         'title': game.name,
-        'items': Item.objects.all().filter(game=game_id)
+        'search_text': 'Search by description',
+        'items': items
+    }
+
+    return render(request, 'product/game.html', context)
+
+
+def sellers_item(request, game_id, seller_name):
+    game = Game.objects.filter(id=game_id).first()
+    context = {
+        'title': game.name,
+        'items': Item.objects.all().filter(game=game_id, seller=seller_name)
     }
     return render(request, 'product/game.html', context)
